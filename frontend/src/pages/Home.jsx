3b6a8a96e076f046558074ed389b9a601bf69ead
Kethan Pilla <kethanpilla11@gmail.com>
2025-05-22 23:19:21 -0500

    Edited Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <div className='h-screen flex justify-between flex-col w-full bg-red-500'>
-            <div className='bg-white'>
-                <h2>Get Started with Uber</h2>
-                <button>Continue</button>
+        <div className='bg-cover bg-[url("https://images.unsplash.com/photo-1593950315186-76a92975b60c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fFViZXIlMjBmbGVldHxlbnwwfHwwfHx8MA%3D%3D")] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+          <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png" alt="uber logo" />
+            <div className='bg-white pb-7 py-4 px-4'>
+                <h2 className='text-3xl font-bold'>Get Started with Uber</h2>
+                <button className='w-full bg-black text-white py-3 rounded mt-5'>Continue</button>
 
             </div>
         </div>
