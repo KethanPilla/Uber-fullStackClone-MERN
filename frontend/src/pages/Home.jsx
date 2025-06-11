d2c12d16b99f8c7ee10674d6d91671ab7e7c2053
Kethan Pilla <kethanpilla11@gmail.com>
2025-06-10 23:46:02 -0400

    debugging Home page and added new features.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { use } from 'react'
 import { useState } from 'react'
+import {useGSAP} from '@gsap/react'
+import gsap from 'gsap'
+import { useRef } from 'react'
 
 const Home = () => {
   const [pickup, setpickup] = useState('')
   const [destination, setdestination] = useState('')
   const [panelOpen, setpanelOpen] = useState(false)
+  const panelRef = useRef(null)
   const submitHandler = (e) => {
     e.preventDefault();
   }
+
+  useGSAP(function() {
+    if(panelOpen){
+      gsap.to(panelRef.current, {
+        height: '70%'
+    })
+    }else{
+      gsap.to(panelRef.current, {
+        height: '0%'
+      })
+    }
+  },[panelOpen])
   return (
     <div className='h-screen relative'>
       <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt='Uber Logo' />
@@ -17,7 +33,7 @@ const Home = () => {
         <img className='w-screen h-screen object-cover' src="https://i.imgur.com/khtan7H.png" alt='Uber vehicle map' />
       </div>
       <div className='flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] p-5 bg-white relative' >
+        <div className='h-[30%] p-6 bg-white relative' >
           <h4 className='text-3xl font-semibold'>Find a trip</h4>
         <form onSubmit={(e)=>{
           submitHandler(e)
@@ -31,13 +47,13 @@ const Home = () => {
           type='text' placeholder='Add a pick-up location'/>
           <input 
           onClick={() => setpanelOpen(true)} 
-          value={pickup}
+          value={destination}
           onChange={(e) => setdestination(e.target.value)}
           className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-5' 
           type='text' placeholder='Enter your destination'/>
           </form>
         </div>
-        <div className='h-[70%] bg-red-500 p-5 h-0'>
+        <div ref={panelRef} className='bg-red-500 h-0'>
 
         </div>
       </div>
